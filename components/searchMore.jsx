427094b20822cff62f781671195ce0a7d59a07b8
annaleexjohnson <87035551+annaleexjohnson@users.noreply.github.com>
2022-07-30 14:52:44 -0500

    Add expanding search bar

--- components/searchMore.jsx
+++ components/searchMore.jsx
@@ -1,11 +1,22 @@
 import styles from '../styles/SearchMore.module.scss'
+import { MagnifyingGlass } from "phosphor-react";
 
 const SearchMore = () => {
     return (
         <div className={styles.searchMore}>
-            <h1>Nothing to your liking?</h1>
-            <button className={styles.searchMore__btn}>Search Recipes</button>
+            <div className={styles.searchMore__container}>
+                <h1>Nothing to your liking?</h1>
+                <span className={styles.searchMore__title}>Search Recipes</span>
+
+                <form className={styles.searchMore__form}>
+                    <input type="text" placeholder="Search for a recipe"className={styles.searchMore__input}/>
+                    <button>
+                        <MagnifyingGlass size={24} weight="bold" color="#fff"/>
+                    </button>
+                </form>
+            </div>
         </div>
+        
     );
 }
 
